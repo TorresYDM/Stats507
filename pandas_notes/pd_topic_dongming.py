4012df341550b7705a49f5d864f41b50110d89aa
TorresYDM <dongming@umich.edu>
2021-11-12 03:17:44 -0500

    clone the file from stats507_F21 copy the file and write into the ps4 q0 tutorial

---
+++ pandas_notes/pd_topic_dongming.py
@@ -0,0 +1,39 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_json: true
+#     notebook_metadata_filter: markdown
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.11.4
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# ## Topics in Pandas
+# **Stats 507, Fall 2021** 
+# 
+# + [Topic dongming dongming@umich]
+# For this question, please pick a topic - such as a function, class, method, recipe or idiom related to the pandas python library and create a short tutorial or overview of that topic. The only rules are below.
+#     1. Pick a topic not covered in the class slides.
+#     2. Do not knowingly pick the same topic as someone else.
+#     3. Use bullet points and titles (level 2 headers) to create the equivalent of 3-5 “slides” of key points. They shouldn’t actually be slides, but please structure your key points in a manner similar to the class slides (viewed as a notebook).
+#     4. Include executable example code in code cells to illustrate your topic.
+# You do not need to clear your topic with me. If you want feedback on your topic choice, please discuss with me or a GSI in office hours.
+# Link: 
+  
+# ## Contents
+# Add a bullet for each topic and link to the level 2 title header using 
+# the exact title with spaces replaced by a dash. 
+#
+# + [Topic Title](#Topic-Title) 
+# + [Topic 2 Title](#Topic-2-Title)
+
+# ## Topic Title
+# Include a title slide with a short title for your content.
+# Write your name in *bold* on your title slide. 
+
